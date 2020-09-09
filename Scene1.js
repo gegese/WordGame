ca8d1a495155ba737b0bfefaa18a266cede1eab3
gegese <mehmet@getbunya.com>
2020-09-09 22:18:19 +0300

    Added letter square layout

--- Scene1.js
+++ Scene1.js
@@ -3,9 +3,28 @@ class Scene1 extends Phaser.Scene {
     super("bootGame");
   }
 
+  init() {
+    var me = this;
+
+    var style = {
+      font: "32px Arial",
+      fill: "#f4f4f4",
+      align: "center"
+    };
+
+    this.text = this.add.text(100, 300, "Loading: 0%", style);
+  }
+
   preload() {
     this.load.image("background", "assets/images/main-background.png");
+    this.load.text('dictionary', 'assets/words.txt');
+    this.load.image("square", "assets/images/letter-square.png");
   }
+
+  fileLoaded(progress) {
+    this.text.text = "Loading:" + progress + "%";
+  }
+
 
   create(){
     this.scene.start("playGame");
--- Scene2.js
+++ Scene2.js
@@ -6,6 +6,38 @@ class Scene2 extends Phaser.Scene {
   create() {
     this.background = this.add.image(0,0,"background");
     this.background.setOrigin(0,0);
+
+    // dama
+    this.letter1 = this.add.image(config.width/2 - 110, config.height/2 - 90, "square");
+    this.letter2 = this.add.image(config.width/2 - 55, config.height/2 - 90, "square");
+    this.letter3 = this.add.image(config.width/2, config.height/2 - 90, "square");
+    this.letter4 = this.add.image(config.width/2 + 55, config.height/2 - 90, "square");
+    // adam
+    this.letter1 = this.add.image(config.width/2 - 55, config.height/2 - 200, "square");
+    this.letter2 = this.add.image(config.width/2, config.height/2 - 200, "square");
+    this.letter3 = this.add.image(config.width/2 + 55, config.height/2 - 200, "square");
+    this.letter4 = this.add.image(config.width/2 + 110, config.height/2 - 200, "square");
+    // dam
+    this.letter3 = this.add.image(config.width/2, config.height/2 - 145, "square");
+    // ama
+    this.letter2 = this.add.image(config.width/2 - 55, config.height/2 - 35, "square");
+    this.letter2 = this.add.image(config.width/2 - 55, config.height/2 + 20 , "square");
+    // ada
+    this.letter2 = this.add.image(config.width/2 + 55, config.height/2 - 35, "square");
+    this.letter2 = this.add.image(config.width/2 + 55, config.height/2 + 20 , "square");
+
+    this.dict = this.add.text("dictionary");
+
+    var text = "adam";
+
+    if(this.cache.text.get('dictionary').indexOf(text) > -1){
+      this.add.text(10, 10, "exist :" + " " + text, {font: "25px Arial", fill: "#ffffff ", align: "center", backgroundColor: "#000000"});
+    }else {
+      // alert("does not exist");
+      this.add.text(20, 20, "does not exist", {font: "25px Arial", fill: "yellow"});
+    }
+
   }
+
 
 }
